9d6abcf8d62ffa5871adf6769c2c06f591de1a0b
emon7766554 <emon6262420@gmail.com>
2025-05-05 10:02:11 +0600

    Add files via upload

---
+++ products.js
@@ -0,0 +1,19 @@
+
+export const products = [
+  {
+    id: 1,
+    name: "Champion Tee",
+    price: "$25.00",
+    description: "Comfortable, durable, and stylish tee.",
+    sizes: ["S", "M", "L", "XL"],
+    image: "/champion.jpg"
+  },
+  {
+    id: 2,
+    name: "Elite Performance Shirt",
+    price: "$30.00",
+    description: "Moisture-wicking athletic wear for peak performance.",
+    sizes: ["M", "L", "XL"],
+    image: "/performance.jpg"
+  }
+];
